3b7f0b615833aac16914f39a7a5b732397c33c90
Alex PC <31523920+Cr-ad@users.noreply.github.com>
2018-03-25 15:23:59 +0100

    Adding pug template engine

--- blog/server.js
+++ blog/server.js
@@ -1,14 +1,21 @@
 const express = require ('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+
+
 const db = require('./config/db');
 
+var currentDate = new Date().toLocaleString();
 
 const blog_app = express();
 
 const port = 3000;
 
 blog_app.use(bodyParser.urlencoded({ extended: true}))
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 MongoClient.connect(db.url, (err, database) => {
     if (err)
@@ -19,7 +26,7 @@ MongoClient.connect(db.url, (err, database) => {
     {
         require('./app/routes')(blog_app, database);
         blog_app.listen(port, () => {
-            console.log("Listening on port " + port + " @ http://localhost:3000");
+            console.log(currentDate + " | Listening on port " + port + " @ http://localhost:3000");
         });
     }
 
