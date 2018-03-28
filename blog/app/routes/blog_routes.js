ceb1ecc7ca6dc97e5347933b1194471b582bdba8
Alex PC <31523920+Cr-ad@users.noreply.github.com>
2018-03-28 13:08:43 +0100

    Removed manual post list - only db posts now

--- blog/app/routes/blog_routes.js
+++ blog/app/routes/blog_routes.js
@@ -17,7 +17,7 @@ module.exports = function(blog_app, client) {
     blog_app.get('/test/', (req, res) => {
         res.render('index', { title : 'The Test Route'});
     });
-    
+
     // Articles Route
     blog_app.get('/articles/', (req, res) => {
         var dbPosts = [];
@@ -25,8 +25,7 @@ module.exports = function(blog_app, client) {
         // Execute the each command, triggers for each document
         cursor.forEach(function(doc, err) {
             assert.equal(null, err);
-            //console.log(" Article Title: " + doc.title)
-            // Add some validation
+            // Need to add some validation
             var post = {
                 id:         doc._id,
                 author:     doc.author,
@@ -50,58 +49,29 @@ module.exports = function(blog_app, client) {
     // Articles Route
     blog_app.get('/articles/test', (req, res) => {
         
-        var dbPosts = new Array();
-
+        var dbPosts = [];
         var cursor = db.collection('posts').find();
         // Execute the each command, triggers for each document
-        cursor.forEach(function(doc, index) {
-            console.log(" Article Title: " + doc.title)
-            dbPosts.push(doc);
-        });
-        /*
-        // Execute the each command, triggers for each document
-        cursor.each(function(err, item) {
-            // If item is null, cursor is empty
-            if (item == null)
-            {
-                cursor.toArray(function(err, items) {
-                    assert.ok(err != null);
-                });
-            };
-        }); */
-        
-        let posts = [
-            {
-                author: "Jim Halpert",
-                title: "Elon Musk Lands on Mars",
-                thumbnail: "/images/tesla_model_s.jpg",
-                content:   "Lorem ipsum dolor sit amet, maluisset salutatus ea mei, id vim erant tincidunt instructior. Probo ornatus partiendo mei eu, nonumy partiendo democritum mei ei, mollis iisque eam no. Quod aeterno malorum ei nam, mel cu partem mandamus. Sit causae iriure at, qui habeo oblique epicuri eu. Mutat facilis id pri, ea qui vocibus volutpat, eruditi noluisse per te.<br /><br />Ea cum quem quod tincidunt, mea latine efficiendi persequeris cu. No sed simul zril regione, nemore epicurei scribentur nec et, aliquid fabulas definiebas te sed. Eam ne saepe perfecto vulputate, iusto homero eu sed, id est dico oblique. Eum ad adhuc scripta. Pro enim brute ea, no nihil iisque mentitum duo, veri ludus dissentiunt ea eam. At omnes concludaturque mea, dicit lucilius reprehendunt mea et, noster animal perfecto no sea.<br /><br />Errem vocent delicatissimi eum an. Et consul denique reformidans eum, quo ea senserit pericula. At sit omnis eirmod interesset, no mei platonem reprimique, ei vix nobis fastidii qualisque. In pro natum omittantur. Mel recusabo repudiare ea.<br /><br />An duo tritani ponderum. Ei perpetua accusamus adolescens mel. Mei cu recusabo consequat, graeco regione in per, ea mel tantas eirmod reprehendunt.",
-                date:   "26/03/2018",
-                category: "Technology",
-                tags: ["Cars", "Tesla", "Elon Musk"]
-            },
-            {
-                author: "Michael Scott",
-                title: "Elon Musk Accidentally Orders 6 Honda Accords",
-                thumbnail: "/images/tesla_model_3.jpg",
-                content:   "Lorem ipsum dolor sit amet, maluisset salutatus ea mei, id vim erant tincidunt instructior. Probo ornatus partiendo mei eu, nonumy partiendo democritum mei ei, mollis iisque eam no. Quod aeterno malorum ei nam, mel cu partem mandamus. Sit causae iriure at, qui habeo oblique epicuri eu. Mutat facilis id pri, ea qui vocibus volutpat, eruditi noluisse per te.<br /><br />Ea cum quem quod tincidunt, mea latine efficiendi persequeris cu. No sed simul zril regione, nemore epicurei scribentur nec et, aliquid fabulas definiebas te sed. Eam ne saepe perfecto vulputate, iusto homero eu sed, id est dico oblique. Eum ad adhuc scripta. Pro enim brute ea, no nihil iisque mentitum duo, veri ludus dissentiunt ea eam. At omnes concludaturque mea, dicit lucilius reprehendunt mea et, noster animal perfecto no sea.<br /><br />Errem vocent delicatissimi eum an. Et consul denique reformidans eum, quo ea senserit pericula. At sit omnis eirmod interesset, no mei platonem reprimique, ei vix nobis fastidii qualisque. In pro natum omittantur. Mel recusabo repudiare ea.<br /><br />An duo tritani ponderum. Ei perpetua accusamus adolescens mel. Mei cu recusabo consequat, graeco regione in per, ea mel tantas eirmod reprehendunt. Duis percipitur neglegentur has ad, idque scripta vix ut. Eu minimum dignissim usu. Nec soluta corpora id.<br /><br />Illud epicuri at vis, est officiis forensibus ex. Ne wisi dicat exerci pri. Usu ut discere voluptua adolescens. Omnium placerat mel ut, te eos dicta suavitate. Ea luptatum sententiae consetetur qui, appetere vituperata ius ne.",
-                date:   "25/03/2018",
-                category: "Automotive",
-                tags: ["Cars", "Tesla", "Elon Musk"]
-            },
-            {
-                author: "Pam Beasley",
-                title: "Elon Musk Finds Out What It Means To Be Human",
-                thumbnail: "/images/tesla_roadster.jpg",
-                content:   "Lorem ipsum dolor sit amet, maluisset salutatus ea mei, id vim erant tincidunt instructior. Probo ornatus partiendo mei eu, nonumy partiendo democritum mei ei, mollis iisque eam no. Quod aeterno malorum ei nam, mel cu partem mandamus. Sit causae iriure at, qui habeo oblique epicuri eu. Mutat facilis id pri, ea qui vocibus volutpat, eruditi noluisse per te.<br /><br />Ea cum quem quod tincidunt, mea latine efficiendi persequeris cu. No sed simul zril regione, nemore epicurei scribentur nec et, aliquid fabulas definiebas te sed. Eam ne saepe perfecto vulputate, iusto homero eu sed, id est dico oblique. Eum ad adhuc scripta. Pro enim brute ea, no nihil iisque mentitum duo, veri ludus dissentiunt ea eam. At omnes concludaturque mea, dicit lucilius reprehendunt mea et, noster animal perfecto no sea.<br /><br />Errem vocent delicatissimi eum an. Et consul denique reformidans eum, quo ea senserit pericula. At sit omnis eirmod interesset, no mei platonem reprimique, ei vix nobis fastidii qualisque. In pro natum omittantur. Mel recusabo repudiare ea.<br /><br />An duo tritani ponderum. Ei perpetua accusamus adolescens mel. Mei cu recusabo consequat, graeco regione in per, ea mel tantas eirmod reprehendunt. Duis percipitur neglegentur has ad, idque scripta vix ut. Eu minimum dignissim usu. Nec soluta corpora id.<br /><br />Illud epicuri at vis, est officiis forensibus ex. Ne wisi dicat exerci pri. Usu ut discere voluptua adolescens. Omnium placerat mel ut, te eos dicta suavitate. Ea luptatum sententiae consetetur qui, appetere vituperata ius ne.",
-                date:   "24/03/2018",
-                category: "Sports",
-                tags: ["Cars", "Tesla", "Elon Musk"]
+        cursor.forEach(function(doc, err) {
+            assert.equal(null, err);
+            // Need to add some validation
+            var post = {
+                id:         doc._id,
+                author:     doc.author,
+                title:      doc.title,
+                thumbnail:  doc.thumbnail,
+                content:    doc.content,
+                date:       doc.date,
+                category:   doc.category,
+                tags:       doc.tags
             }
-        ];
-        res.render('article', {
-            title : 'The Article Route',
-            "posts": posts
+            dbPosts.push(post);
+            console.log("ID: " + post.id +  " | Title: " + post.title);
+        }, function() {
+                res.render('article', {
+                    title : 'The Articles Route',
+                    "posts": dbPosts
+                });
         });
     });
 
