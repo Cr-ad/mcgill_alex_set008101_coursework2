24467c3bf7df5d1499bcdd91983056f09b0d9b00
Alex <31523920+Cr-ad@users.noreply.github.com>
2018-04-02 23:20:12 +0100

    Added different validaton for new article submisisons

--- blog/app/js/scripts.js
+++ blog/app/js/scripts.js
@@ -1,47 +1,17 @@
 function validateBlogPostSubmission()
 {
     // Length more than 1
-    var title = document.forms["add_blog_post"]["title"].value;
-    var titleFiltered; // keep removing special characters from first position
-    // Length more than 1
-    var content;
-    // At least 1 individual tag
-    var tags;
-    var tagArray = [];
-    // Only 1 category
-    // not needed, form going to be a drop down selection.
-    var validCategories = ["technology", "automotive", "sports", "business", "misc"];
-    var category;
-    // Length more than 1
-    var author;
-
-
-    if(title.length < 2)
+    var category = document.forms["add_blog_post"]["category"].value;
+    if(category === "default")
     {
-        alert("Error: Please enter a valid title");
+        alert("Please select a category");
         return false;
     }
-    else if(content.length < 2)
+    else
     {
-        alert("Error: Please enter some valid content");
-        return false;
+        return true;
     }
-    else if (tagArray.length < 1)
-    {
-        alert("Error: Please enter at least one tag");
-        return false;
-    }
-    //else if(!validCategories.contains(category.toLowerCase()))
-    {
-        alert("Error: Please choose a valid category");
-        return false;
-    }
-    var x = true;
-    if (x)
-    {
-        alert("Data is not valid | TITLE: " + title);
-        return false;
-    }
+    
 }
 
 function test()
