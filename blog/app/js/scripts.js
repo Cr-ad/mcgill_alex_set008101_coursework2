c3716353e8162f6b715163fec84395786883cbd3
Alex <31523920+Cr-ad@users.noreply.github.com>
2018-04-03 20:54:29 +0100

    Added title character limit, lots of minor url related changes

--- blog/app/js/scripts.js
+++ blog/app/js/scripts.js
@@ -2,9 +2,16 @@ function validateBlogPostSubmission()
 {
     // Length more than 1
     var category = document.forms["add_blog_post"]["category"].value;
+    var title = document.forms["add_blog_post"]["title"].value;
+    var maxTitleLength = 60;
     if(category === "default")
     {
         alert("Please select a category");
+        return false;
+    }
+    else if(title.length > maxTitleLength)
+    {
+        alert("Title has too many characters ("+title.length+"). Limit: " + maxTitleLength);
         return false;
     }
     else
