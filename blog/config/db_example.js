3121cb0b4818e0ef9cdfaa9e5714bb2c91501b91
Alex PC <31523920+Cr-ad@users.noreply.github.com>
2018-05-30 16:44:06 +0100

    Added demo images

---
+++ blog/config/db_example.js
@@ -0,0 +1,12 @@
+// Your file should be name db.js with the following content:
+
+module.exports = {
+    url : "YOUR_DB_CONNECTION_INFO"
+}
+
+/* For example:
+
+module.exports = {
+    url : "mongodb://<dbuser>:<dbpassword>@ds012345.mlab.com:56789/mydb"
+}
+*/
